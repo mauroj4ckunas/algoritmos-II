ef42e6a43bfae7b276dc50f7af67209219f27cf5
mauroj4ckunas <96546837+mauroj4ckunas@users.noreply.github.com>
2022-09-28 20:59:10 -0300

    Update lista_enlazada.go

--- lista_enlazada.go
+++ lista_enlazada.go
@@ -12,18 +12,18 @@ type listaEnlazada[T any] struct {
 }
 
 type iteradorExterno[T any] struct {
-	actual   *nodoLista[T]
-	anterior *nodoLista[T]
-	proximo  *nodoLista[T]
+	actual       *nodoLista[T]
+	anterior     *nodoLista[T]
+	listaExterna *listaEnlazada[T]
 }
 
 /* Primitivas de Lista */
 
-func (lista listaEnlazada[T]) EstaVacia() bool {
+func (lista *listaEnlazada[T]) EstaVacia() bool {
 	return lista.largo == 0
 }
 
-func (lista listaEnlazada[T]) InsertarPrimero(elem T) {
+func (lista *listaEnlazada[T]) InsertarPrimero(elem T) {
 	nuevo := crearNodo[T](elem)
 	if lista.EstaVacia() {
 		iniciarLista(lista, nuevo)
@@ -34,7 +34,7 @@ func (lista listaEnlazada[T]) InsertarPrimero(elem T) {
 	lista.largo++
 }
 
-func (lista listaEnlazada[T]) InsertarUltimo(elem T) {
+func (lista *listaEnlazada[T]) InsertarUltimo(elem T) {
 	nuevo := crearNodo[T](elem)
 	if lista.EstaVacia() {
 		iniciarLista(lista, nuevo)
@@ -45,21 +45,21 @@ func (lista listaEnlazada[T]) InsertarUltimo(elem T) {
 	lista.largo++
 }
 
-func (lista listaEnlazada[T]) VerPrimero() T {
+func (lista *listaEnlazada[T]) VerPrimero() T {
 	if lista.EstaVacia() {
 		panic("La lista esta vacia")
 	}
 	return lista.primero.dato
 }
 
-func (lista listaEnlazada[T]) VerUltimo() T {
+func (lista *listaEnlazada[T]) VerUltimo() T {
 	if lista.EstaVacia() {
 		panic("La lista esta vacia")
 	}
 	return lista.ultimo.dato
 }
 
-func (lista listaEnlazada[T]) BorrarPrimero() T {
+func (lista *listaEnlazada[T]) BorrarPrimero() T {
 	if lista.EstaVacia() {
 		panic("La lista esta vacia")
 	}
@@ -74,7 +74,7 @@ func (lista listaEnlazada[T]) BorrarPrimero() T {
 	return datoPrimero
 }
 
-func (lista listaEnlazada[T]) Largo() int {
+func (lista *listaEnlazada[T]) Largo() int {
 	return lista.largo
 }
 
@@ -85,56 +85,82 @@ func (lista listaEnlazada[T]) Largo() int {
 
 /* Primitivas de los iteradores */
 
-func (iter iteradorExterno[T]) VerActual() T {
-	if iter.actual == nil {
-		panic("El iterador termino de iterar")
-	}
+func (iter *iteradorExterno[T]) VerActual() T {
+	findeIteracion(iter)
 	return iter.actual.dato
 }
 
-func (iter iteradorExterno[T]) HaySiguiente() bool {
-	return iter.proximo != nil
+func (iter *iteradorExterno[T]) HaySiguiente() bool {
+	return iter.actual.siguiente != nil
 }
 
-func (iter iteradorExterno[T]) Siguiente() T {
+func (iter *iteradorExterno[T]) Siguiente() T {
+	findeIteracion(iter)
 	datoActual := iter.VerActual()
-	iter.anterior = iter.actual
-	iter.actual = iter.proximo
-	iter.proximo = iter.proximo.siguiente
+	if iter.HaySiguiente() {
+		iter.anterior = iter.actual
+		iter.actual = iter.actual.siguiente
+		iter.actual.siguiente = iter.actual.siguiente.siguiente
+	} else {
+		iter.actual = nil
+	}
+
 	return datoActual
 }
 
-// El iterador va bien pero no esta modificando la lista original
-
-func (iter iteradorExterno[T]) Insertar(elem T) {
+func (iter *iteradorExterno[T]) Insertar(elem T) {
 	nuevo := crearNodo[T](elem)
-	nuevo.siguiente = iter.actual
-	iter.anterior.siguiente = nuevo
-	iter.proximo = iter.actual
+
+	if iter.anterior == nil {
+		iter.listaExterna.primero = nuevo
+		if iter.listaExterna.largo == 0 {
+			iter.listaExterna.ultimo = iter.listaExterna.primero
+		}
+	} else {
+		nuevo.siguiente = iter.actual
+		iter.anterior.siguiente = nuevo
+		iter.actual.siguiente = iter.actual
+	}
 	iter.actual = nuevo
+	iter.listaExterna.largo++
 }
 
-func (iter iteradorExterno[T]) Borrar() T {
+func (iter *iteradorExterno[T]) Borrar() T {
+	findeIteracion(iter)
 	datoFuera := iter.VerActual()
-	iter.anterior.siguiente = iter.proximo
-	iter.actual = iter.proximo
-	iter.proximo = iter.proximo.siguiente
+
+	if iter.listaExterna.primero == iter.listaExterna.ultimo {
+		iter.listaExterna.primero = nil
+		iter.listaExterna.ultimo = nil
+	} else {
+		iter.anterior.siguiente = iter.actual.siguiente
+		iter.actual = iter.actual.siguiente
+		iter.actual.siguiente = iter.actual.siguiente.siguiente
+	}
+	iter.listaExterna.largo--
 	return datoFuera
 }
 
-func (lista listaEnlazada[T]) Iterador() IteradorLista[T] {
+func (lista *listaEnlazada[T]) Iterador() IteradorLista[T] {
 	iterador := new(iteradorExterno[T])
 	iterador.actual = lista.primero
-	iterador.proximo = lista.primero.siguiente
+	iterador.listaExterna = lista
+	return iterador
 }
 
-func (lista listaEnlazada[T]) Iterar(visitar func(T) bool) {
+func (lista *listaEnlazada[T]) Iterar(visitar func(T) bool) {
 
 }
 
 // Funciones auxiliares
 
-func iniciarLista[T any](listaVacia listaEnlazada[T], nodo *nodoLista[T]) {
+func findeIteracion[T any](iterador *iteradorExterno[T]) {
+	if iterador.actual == nil {
+		panic("El iterador termino de iterar")
+	}
+}
+
+func iniciarLista[T any](listaVacia *listaEnlazada[T], nodo *nodoLista[T]) {
 	listaVacia.primero = nodo
 	listaVacia.ultimo = nodo
 }
