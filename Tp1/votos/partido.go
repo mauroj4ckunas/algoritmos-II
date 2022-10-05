18f3b086f251f38ff536d74bfee8c6effa1d9509
Mauro J <mauronjackunas@gmail.com>
2022-10-05 14:15:56 -0300

    Implementacion de partidos hecho

--- Tp1/votos/partido.go
+++ Tp1/votos/partido.go
@@ -1,6 +1,6 @@
 package votos
 
-//Partido modela un partido político, con sus alternativas para cada uno de los tipos de votaciones
+// Partido modela un partido político, con sus alternativas para cada uno de los tipos de votaciones
 type Partido interface {
 
 	//VotadoPara indica que este Partido ha recibido un voto para el TipoVoto indicado. Felicitaciones!
@@ -8,5 +8,5 @@ type Partido interface {
 
 	//ObtenerResultado permite obtener el resultado de este Partido para el TipoVoto indicado. El formato será el
 	//conveniente para ser mostrado.
-	ObtenerResultado(tipo TipoVoto) string
+	ObtenerResultado(tipo TipoVoto) (string, int)
 }
--- Tp1/votos/partido_implementacion.go
+++ Tp1/votos/partido_implementacion.go
@@ -1,25 +1,60 @@
 package votos
 
 type partidoImplementacion struct {
-	nombre_part		string
-	presidente		*candidatos
-	gobernador		*candidatos
-	intendente		*candidatos
+	nombre_part string
+	presidente  *candidatosParaEleccion
+	gobernador  *candidatosParaEleccion
+	intendente  *candidatosParaEleccion
 }
 
-type candidatos struct {
-	nombre		string
-	cant_votos	int
+type candidatosParaEleccion struct {
+	nombre     string
+	cant_votos int
 }
 
 func CrearPartido(nombre string, candidatos [CANT_VOTACION]string) Partido {
-	return nil
+	nuevoPartido := new(partidoImplementacion)
+	nuevoPartido.nombre_part = nombre
+	nuevoPartido.inscribirCandidatos(candidatos)
+	return nuevoPartido
 }
 
 func (partido *partidoImplementacion) VotadoPara(tipo TipoVoto) {
-
+	if tipo == 0 {
+		partido.presidente.cant_votos++
+	} else if tipo == 1 {
+		partido.gobernador.cant_votos++
+	} else if tipo == 2 {
+		partido.intendente.cant_votos++
+	}
 }
 
-func (partido partidoImplementacion) ObtenerResultado(tipo TipoVoto) string {
-	return ""
+func (partido partidoImplementacion) ObtenerResultado(tipo TipoVoto) (string, int) {
+	var nombreCandidato string
+	var cantidadVotos int
+	if tipo == 0 {
+		nombreCandidato = partido.presidente.nombre
+		cantidadVotos = partido.presidente.cant_votos
+	} else if tipo == 1 {
+		nombreCandidato = partido.gobernador.nombre
+		cantidadVotos = partido.gobernador.cant_votos
+	} else if tipo == 2 {
+		nombreCandidato = partido.intendente.nombre
+		cantidadVotos = partido.intendente.cant_votos
+	}
+	return nombreCandidato, cantidadVotos
+}
+
+func (partido partidoImplementacion) inscribirCandidatos(candidatos [CANT_VOTACION]string) {
+	candidatoPres := new(candidatosParaEleccion)
+	candidatoPres.nombre = candidatos[0]
+	partido.presidente = candidatoPres
+
+	candidatoGob := new(candidatosParaEleccion)
+	candidatoGob.nombre = candidatos[1]
+	partido.presidente = candidatoGob
+
+	candidatoInt := new(candidatosParaEleccion)
+	candidatoInt.nombre = candidatos[2]
+	partido.intendente = candidatoInt
 }
