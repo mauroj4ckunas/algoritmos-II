81f461bd4613f8952ee7de572c1c52203727db67
mauroj4ckunas <96546837+mauroj4ckunas@users.noreply.github.com>
2022-10-04 23:34:26 -0300

    Cambio en el Crear Partido

--- Tp1/votos/partido_implementacion.go
+++ Tp1/votos/partido_implementacion.go
@@ -13,7 +13,10 @@ type candidatos struct {
 }
 
 func CrearPartido(nombre string, candidatos [CANT_VOTACION]string) Partido {
-	return nil
+	nuevoPartido := new(partidoImplementacion)
+	nuevoPartido.nombre_part = nombre
+	nuevoPartido.inscribirCandidatos(candidatos)
+	return nuevoPartido
 }
 
 func (partido *partidoImplementacion) VotadoPara(tipo TipoVoto) {
@@ -22,4 +25,18 @@ func (partido *partidoImplementacion) VotadoPara(tipo TipoVoto) {
 
 func (partido partidoImplementacion) ObtenerResultado(tipo TipoVoto) string {
 	return ""
+}
+
+func (partido partidoImplementacion) inscribirCandidatos(candidatos [CANT_VOTACION]string) {
+	candidatoPres := new(candidatosParaEleccion)
+	candidatoPres.nombre = candidatos[0]
+	partido.presidente = candidatoPres
+
+	candidatoGob := new(candidatosParaEleccion)
+	candidatoGob.nombre = candidatos[1]
+	partido.presidente = candidatoGob
+
+	candidatoInt := new(candidatosParaEleccion)
+	candidatoInt.nombre = candidatos[2]
+	partido.intendente = candidatoInt
 }
