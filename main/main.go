dba1a489162e0e4dcfa16f7128eb13fe0776b025
FacuGerez <facundogerezuba@hotmail.com>
2022-10-06 09:42:26 -0300

    main explicado

--- main/main.go
+++ main/main.go
@@ -10,20 +10,23 @@ import (
 
 func main() {
 
-	parametros := os.Args[1:]
-	if len(parametros) != 2 {
+	parametros := os.Args[1:] //recibe los nombres de los archivos pasados por parametro en un array
+							  // el [1:] es para sacar el nombre del archivo (main)
+	
+	if len(parametros) != 2 { //si los parametros son mas chicos que dos, error
 		var error ErrorParametros = ErrorParametros
 		return error.Error()
 	}
 
-	rutaListas := parametros[0]
-	rutaPadrones := parametros[1]
+	rutaListas := parametros[0]    //el primer parametro era el nombre del archivo de las listas
+	rutaPadrones := parametros[1]  //el segundo el de los padrones
 
-	pila := TDAPila.CrearPilaDinamica[string]()
+	pila := TDAPila.CrearPilaDinamica[string]() // esto es para usarlo para crear los array de los partidos y los participantes
 
 	archivoListas, err := os.Open(rutaListas)
 	defer archivoListas.Close()
-	if err != nil {
+	
+	if err != nil { //si la ruta no se puede leer o algo, error
 		var error ErrorLeerArchivo = ErrorLeerArchivo
 		return error.Error()
 	}
