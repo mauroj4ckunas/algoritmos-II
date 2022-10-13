00c943e6c90856b9f54564baf38572d1441e03ed
FacuGerez <facundogerezuba@hotmail.com>
2022-10-13 18:03:00 -0300

    Escritura pseudocodigo

--- hashes/diccionario_implementacion.go
+++ hashes/diccionario_implementacion.go
@@ -14,15 +14,17 @@ func hashear[K comparable](clave K) uint64 {
 }
 
 
-type elementos[K comparable, V any] struct{
-	clave 	K
-	valor 	V
+type elementos[K comparable, V any] struct {
+	clave 		K
+	valor 		V
+	ubicacion 	int 
 }
 
-func crearElemento [K comparable, V any] (clave K, valor V) elementos[K,V] {
+func crearElemento [K comparable, V any] (clave K, valor V,ubicacionHash int) elementos[K,V] {
 	dato := new(elementos[K,V])
 	dato.clave = clave
 	dato.valor = valor
+	dato.ubicacion = ubicacionHash
 	return dato
 }
 
@@ -33,8 +35,8 @@ type diccionario_implementacion[K comparable, V any] struct {
 }
 
 func (dicc *diccionario_implementacion) Guardar(clave K, dato V) {
-	paraGuardar := crearElemento[K,V](clave,dato)
 	ubicacion := hashear[K](clave) % len(dicc.array)
+	paraGuardar := crearElemento[K,V](clave,dato,ubicacion)
 	var i int
 	for i = ubicacion ; i < (ubicacion + 3) ; i++ {
 		if dicc.array[i] == nil {
@@ -43,7 +45,11 @@ func (dicc *diccionario_implementacion) Guardar(clave K, dato V) {
 	}
 
 	if i == (ubicacion + 3){
-		// implementar el hacer espacio y el redimensionar
+		// Buscar el mas cercano espacio libre
+		// buscar 2 arriba del espacio libre si alguno puede moverse
+		// si se puede se hace un swap y luego se fija si se puede poner el nuevo elemento
+		//en caso de q no se pueda se vuelve a hacer lo mismo
+		// y en caso de no poder mover nada se redimensiona
 	}
 }
 
