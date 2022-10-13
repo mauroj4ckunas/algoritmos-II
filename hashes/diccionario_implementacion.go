0063fe6e41d4280b29a9a9ce1945c4bd39b1d17c
FacuGerez <facundogerezuba@hotmail.com>
2022-10-13 19:52:24 -0300

    Para revisar

--- hashes/diccionario_implementacion.go
+++ hashes/diccionario_implementacion.go
@@ -5,7 +5,7 @@ import (
 	"crypto/md5"
 	"encoding/binary"
 )
-
+//funcion de hash
 func hashear[K comparable](clave K) uint64 {
 	elementoHasheable := []byte(fmt.Sprintf("%v", clave))
 	hasheado := md5.Sum(elementoHasheable)
@@ -13,13 +13,13 @@ func hashear[K comparable](clave K) uint64 {
 	return binary.BigEndian.Uint64(convertirAEntero)
 }
 
-
+//celdas del diccionario
 type elementos[K comparable, V any] struct {
 	clave 		K
 	valor 		V
 	ubicacion 	int 
 }
-
+//creador de celdas
 func crearElemento [K comparable, V any] (clave K, valor V,ubicacionHash int) elementos[K,V] {
 	dato := new(elementos[K,V])
 	dato.clave = clave
@@ -28,13 +28,41 @@ func crearElemento [K comparable, V any] (clave K, valor V,ubicacionHash int) el
 	return dato
 }
 
-
+//diccionario
 type diccionario_implementacion[K comparable, V any] struct {
 	array		[]elementos[K,V]
 	largo		int
+	factorCarga	float32
+}
+
+//creador del diccionario
+func CrearDiccionario [K comparable, V any] () Diccionario[K, V] {
+	diccio := new(diccionario_implementacion[K,V])
+	diccio.array := make([]elementos[K,V],100)
+	return diccio
+}
+
+//iterador externo del diccionario
+type iterador_externo[K comparable, V any] struct {
+	actual 		int
+	dicc 		[]elementos[K,V]
+}
+
+//creador de iterador externo
+func crearIteradorExterno [K comparable, V any] (dicc diccionario_implementacion[K,V]) IterDiccionario[K, V] {
+	iterr := new(iterador_externo[K,V])
+	iterr.dicc = dicc.array
+	for iterr.actual < len(iterr.dicc){
+		if dicc.array[iterr.actual] == nil{
+			iterr.actual++
+		}
+		break
+	}
+	return iterr
 }
 
 func (dicc *diccionario_implementacion) Guardar(clave K, dato V) {
+	dicc.factorCarga = dicc.Cantidad()/len(dicc.array)
 	ubicacion := hashear[K](clave) % len(dicc.array)
 	paraGuardar := crearElemento[K,V](clave,dato,ubicacion)
 	var i int
@@ -90,3 +118,32 @@ func (dicc *diccionario_implementacion) Borrar(clave K) V {
 func (dicc *diccionario_implementacion) Cantidad() int {
 	return dicc.largo
 }
+/*
+func (dicc *diccionario_implementacion) Iterador() IterDiccionario[K, V] {
+	return crearIteradorExterno [K, V] (dicc)
+}
+
+func (iterr *iterador_externo) HaySiguiente() bool {
+	return iterr.actual < len(iterr.dicc)
+}
+
+func (iterr *iterador_externo) VerActual() (K, V){
+	if !iterr.HaySiguiente(){
+		panic("El iterador termino de iterar")
+	}
+	return iterr.dicc[iterr.actual].clave , iterr.dicc[iterr.actual].valor
+}
+
+func (iterr *iterador_externo) Siguiente() K {
+	if !iterr.HaySiguiente(){
+		panic("El iterador termino de iterar")
+	}
+	devolver := iterr.dicc[iterr.actual].clave
+	iterr.actual++
+	for iterr.HaySiguiente() {
+		if dicc.array[iterr.actual] == nil{
+			iterr.actual++
+		}
+		break
+	}
+}*/
