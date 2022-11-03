446352a82a84da87826d5fa7cc9f18485ca53b5b
Mauro J <mauronjackunas@gmail.com>
2022-11-03 00:03:31 -0300

    tests

--- cola_prioridad_test.go
+++ cola_prioridad_test.go
@@ -3,8 +3,19 @@ package cola_prioridad_test
 import (
 	TDAHeap "cola_prioridad"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestHeapVacio(t *testing.T) {
-	heap := TDAHeap.CrearHeap[int]()
+	comparar := func(clave1 int, clave2 int) int {
+		if clave1 < clave2 {
+			return -1
+		} else if clave1 > clave2 {
+			return 1
+		}
+		return 0
+	}
+	heap := TDAHeap.CrearHeap[int](comparar)
+	require.True(t, heap.EstaVacia())
 }
