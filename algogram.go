1d940550a37fdeb32d46ae366f2f9eabab3cb567
FacuGerez <facundogerezuba@hotmail.com>
2022-11-05 11:06:02 -0300

    Pseudocodigo en proceso

--- algogram.go
+++ algogram.go
@@ -1,11 +1,5 @@
 package main
 
-
-func crearRedSocial(nombreArchivo string) diccionario{
-	archivoListas, err := os.Open(ruta)
-	defer archivoListas.Close()
-}
-
 const(
 	COMANDO1 = "login"
 	COMANDO2 = "logout"
@@ -15,34 +9,41 @@ const(
 
 func main(){
 	archivoUsuarios := os.Args[1:]
-	TDAAlgogram := crearRedSocial(archivoUsuarios)
+	TDAAlgogram := crearRedSocial(archivoUsuarios[0])
 
-	logeados := TDAcola.CrearCola()
+	logeado := TDAcola.CrearCola()
 	entradaUsuario := bufio.NewScanner(os.Stdin)
 	for entradaUsuario.Scan() {
 		comandos := strings.Split(entradaUsuario.Text(), " ")
 		switch comandos[0] {
 		case COMANDO1:
 			usuario := comandos[1]
-			err, elUsuario:= TDAAlgogram.Login(usuario)
-			if err != nil{
-				fmt.Fprintf(os.Stdout, "%s\n", err.Error())
+			if logeado.EstaVacia(){
+				err, elUsuario:= TDAAlgogram.Login(usuario)
+				if err != nil{
+					fmt.Fprintf(os.Stdout, "%s\n", err.Error())
+				}
+				logeado.Encolar(elUsuario)
 			}else{
-				logeados.Encolar(elUsuario)
+				fmt.Fprintf(os.Stdout, "%s\n", err.Error())
 			}
 			
 		case COMANDO2:
-			if !logeados.EstaVacia(){
-				logeados.VerTope().Logout()
-				logeados.Desencolar()
+			if !logeado.EstaVacia(){
+				logeado.VerTope().Logout()
+				logeado.Desencolar()
 			} else {
 				fmt.Fprintf(os.Stdout, "%s\n", new(err.TALERROR).Error())
 			}
 
 		case COMANDO3:
 			post := comandos[1:]
-			if !logeados.EstaVacia(){
-				TDAAlgogram.Publicar(logeados.VerTope(),post)
+			if !logeado.EstaVacia(){
+				losUsuarios := TDAAlgogram.Iterador() 
+				for losUsuarios.HaySiguiente() {
+					_ ,usuario := losUsuarios.VerActual()
+					usuario.Publicar(post)
+				}
 				fmt.Fprintf(os.Stdout, "%s\n", "Post publicado")
 			} else {
 				fmt.Fprintf(os.Stdout, "%s\n", new(err.TALERROR).Error())
