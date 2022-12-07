83bc04ee9f59927a6e4bbd3e59592af6caf5d048
Mauro J <mauronjackunas@gmail.com>
2022-12-06 23:19:20 -0300

    borrado comentario viejo

--- tp3facu/vamosmoshi.py
+++ tp3facu/vamosmoshi.py
@@ -94,7 +94,7 @@ def guardarCaminoMinimo(grafo: gf.Grafo, desde, hasta, nombreArchivo, coordenada
     except KeyError: #Si el hasta no tiene  
         print(ErrorSinRecorrido().Error())
 
-def reconstruirComando(grafoMundial:gf.Grafo,comando: list)-> (str,str,str):
+def reconstruirComando(grafoMundial:gf.Grafo,comando: list):
     index = 0
     primerElemento = comando[index].replace(",", "")
     while not grafoMundial.pertenece(primerElemento):
@@ -181,41 +181,4 @@ def main():
         else:
             programa = False
 
-
 main()
-
-
-
-
-
-# grafo = gf.Grafo()
-
-# grafo.agregarVertice(1)
-# grafo.agregarVertice(2)
-# grafo.agregarVertice(3)
-# grafo.agregarVertice(4)
-# grafo.agregarVertice(5)
-# grafo.agregarVertice(6)
-
-# grafo.agregarArista(1, 2)
-# grafo.agregarArista(1, 3)
-# grafo.agregarArista(1, 4)
-# grafo.agregarArista(1, 5)
-# grafo.agregarArista(2, 3)
-# grafo.agregarArista(2, 4)
-# grafo.agregarArista(2, 5)
-# grafo.agregarArista(3, 5)
-# grafo.agregarArista(3, 4)
-# grafo.agregarArista(6, 4)
-# grafo.agregarArista(6, 5)
-# grafo.agregarArista(6, 1)
-
-
-# grafoEuler = eu.Euler(grafo)
-
-# print(grafoEuler.cicloEulerianoHierholzer(1))
-# print(grafoEuler.cicloEulerianoHierholzer(2))
-# print(grafoEuler.cicloEulerianoHierholzer(6))
-# print(grafoEuler.cicloEulerianoHierholzer(3))
-
-
